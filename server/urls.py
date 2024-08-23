de1d6ff64aee2d1db49298c4ac1039f4c40f3770
Aldeander <aldeander-santos@hotmail.com>
2024-08-22 23:20:06 -0300

    urls servers api

--- server/urls.py
+++ server/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('compartilhar.urls')),
+    # path('api/', include('compartilhar.urls')),  # Adicione a URL para a API
+    path('', include('compartilhar.urls')),  # Rotas do site
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
